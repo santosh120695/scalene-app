fc563ba1e79f039b08fc1a6bd4bea85e1c42d8af
Santosh ghode <santoshghode@Santoshs-MacBook-Pro.local>
2026-07-13 02:34:54 +0530

    made brand color changes

--- web/dev-dist/sw.js
+++ web/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-8d0d8005'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.364ma9f9048"
+    "revision": "0.b6dkcae3dq"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
