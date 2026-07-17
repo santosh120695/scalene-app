744b79d1628ad6eea967fadb035c252735c22978
Santosh ghode <santoshghode@Santoshs-MacBook-Pro.local>
2026-07-17 12:07:27 +0530

    refactored editor css

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/globals.css";
+import "./styles/editor.css";
+import "./styles/note-preview.css";
 import "./styles/journal.css";
 import "animate.css";
 
